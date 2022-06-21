f63731c91edddb2c6894f0982b31a7a3445666da
alfian <st118436@alumni.ait.ac.th>
2022-06-21 18:35:46 +0700

    Initial project of WhatAreDelegates

---
+++ WhatAreDelegates/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace WhatAreDelegates
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var processor = new PhotoProcessor();
+            processor.Process("photo.js");
+        }
+    }
+}
